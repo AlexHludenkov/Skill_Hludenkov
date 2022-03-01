228b0d700ed58a32d071b34cdc86ebd6c9fe1adb
model3d <model3d@mail.ru>
2022-03-01 18:12:07 +0300

    fics_1

---
+++ 18-5-Code-3/18-5-Code-3/18-5-Code-3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int ways(int a) {
+	int count = 0;
+	int step = 1;
+	if (a == 0) return 1;
+	while (step != 4) {
+		if (step <= a) {
+			count += ways(a - step);
+		}
+		step++;
+	}
+	return count;
+}
+
+int main() {
+	int steps;
+	cout << "Enter number of steps: ";
+	cin >> steps;
+	cout << ways(steps);
+	return 0;
+}
