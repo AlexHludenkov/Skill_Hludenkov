2efd487f8f905c4fee937051e7d57fd5a4de3026
model3d <model3d@mail.ru>
2022-03-01 22:27:12 +0300

    fics 2

--- 18-5-Code-3/18-5-Code-3/18-5-Code-3.cpp
+++ 18-5-Code-3/18-5-Code-3/18-5-Code-3.cpp
@@ -1,11 +1,11 @@
 ﻿#include <iostream>
 using namespace std;
 
-int ways(int a) {
+int ways(int a, int jump = 3) {
 	int count = 0;
 	int step = 1;
 	if (a == 0) return 1;
-	while (step != 4) {
+	while (step != jump) {
 		if (step <= a) {
 			count += ways(a - step);
 		}
@@ -15,9 +15,13 @@ int ways(int a) {
 }
 
 int main() {
-	int steps;
+	int steps, jump;
 	cout << "Enter number of steps: ";
 	cin >> steps;
-	cout << ways(steps);
+	cout << "Enter max jump: ";
+	cin >> jump;
+
+
+	cout << ways(steps, jump);
 	return 0;
 }
