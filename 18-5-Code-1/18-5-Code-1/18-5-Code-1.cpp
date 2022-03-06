b2c3dc000989cc2e1342cbae90e0b4b4eea1a876
model3d <model3d@mail.ru>
2022-03-06 20:02:28 +0300

    fics 3

--- 18-5-Code-1/18-5-Code-1/18-5-Code-1.cpp
+++ 18-5-Code-1/18-5-Code-1/18-5-Code-1.cpp
@@ -4,13 +4,17 @@
 
 using namespace std;
 
-void swapvec() {
-
+void swapvec(vector  <int> *vect, int *arr) {
+	cout << vect;
+	cout << arr;
 }
 
 int main()
 {
-	vector <int> vect = { 1, 2, 3, 4 };
+	vector <int> vect = { 1, 2, 3, 4, 5 };
+	int arr[] = { 6, 7, 8, 9, 10 };
+	swapvec(&vect, arr);
+
 
 
 }
