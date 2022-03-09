cd39fcdfbfcd2683d5783d32787b62b9080e613e
model3d <model3d@mail.ru>
2022-03-09 18:27:49 +0300

    fics

--- 18-5-Code-1/18-5-Code-1/18-5-Code-1.cpp
+++ 18-5-Code-1/18-5-Code-1/18-5-Code-1.cpp
@@ -3,17 +3,23 @@
 
 using namespace std;
 
-void swapvec(vector  <int> *vect_, int *arr) {
-	int i = vect_-> back();
-	cout << i << endl;
+void swapvec(int *vect, int *arr) {
+	
+
+
+	cout << *vect << endl;
 	cout << *arr << endl;
 	cout <<  *(++ arr) << endl;
 	cout << *(++arr) << endl;
+	
 }
 
 int main()
 {
 	vector <int> vect = { 1, 2, 3, 4, 5 };
+	int *ptr = vect.data();
 	int arr[] = { 6, 15, 8, 9, 10 };
-	swapvec(&vect, arr);
+	swapvec(ptr, arr);
+	
+	cout << vect[0];
 }
